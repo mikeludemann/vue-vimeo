6de7469c7da496cc1a7ccee76789e9e3a7562657
Mike Ludemann <mike-ludemann@hotmail.de>
2021-01-19 17:28:45 +0100

    Setup complete - vue-vimeo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vue-vimeo
+
 Vue Vimeo Component
+
+## Properties
+
+### Vimeo
+
+|Key|Value|Type|
+|:--|:----|:---|
+|videoid|The ID of this video|String|
+|width|Width of this element|String|
+|height|Height of this element|String|
---
+++ src/components/vimeo.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+	<iframe id="vimeo" :width="width" :height="height" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</template>
+
+<script lang="js">
+	export default {
+		name: 'vimeo',
+		props: {
+			videoid: String,
+			width: String,
+			height: String
+		},
+		mounted() {
+			this.insertExternalSource();
+		},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			insertExternalSource: function () {
+
+				var video = document.getElementById("vimeo");
+				video.setAttribute("src", "https://player.vimeo.com/video/" + this.videoid);
+
+			}
+		},
+		computed: {
+
+		}
+}
+</script>
+
+<style scoped lang="scss">
+	.vimeo {
+
+	}
+</style>
